d536989904d3dcb68ac6ca255f7d997b5571c6d5
r.piccirillo <r.piccirillo@mseclab.com>
2013-11-13 01:40:36 +0100

    Cifratura e Decifratura

--- gen/com/mseclab/devfest/androidkeystoredemostep4/R.java
+++ gen/com/mseclab/devfest/androidkeystoredemostep4/R.java
@@ -27,14 +27,14 @@ public final class R {
     public static final class id {
         public static final int action_settings=0x7f080009;
         public static final int buttons_bar=0x7f080001;
+        public static final int cifra_button=0x7f080003;
         public static final int container=0x7f080000;
         public static final int debugText=0x7f080008;
-        public static final int firma_button=0x7f080003;
+        public static final int decifra_button=0x7f080004;
         public static final int generate_button=0x7f080002;
         public static final int inDataText=0x7f080005;
         public static final int outDataText=0x7f080006;
         public static final int scrollView=0x7f080007;
-        public static final int verifica_button=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -46,12 +46,12 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
-        public static final int firma_label=0x7f050004;
+        public static final int cifra_label=0x7f050004;
+        public static final int decifra_label=0x7f050005;
         public static final int genera_chiavi=0x7f050003;
         public static final int hello_world=0x7f050001;
         public static final int in_data=0x7f050006;
         public static final int out_data=0x7f050007;
-        public static final int verifica_label=0x7f050005;
     }
     public static final class style {
         /** 
--- src/com/mseclab/devfest/androidkeystoredemostep4/MainActivity.java
+++ src/com/mseclab/devfest/androidkeystoredemostep4/MainActivity.java
@@ -1,4 +1,4 @@
-package com.mseclab.devfest.androidkeystoredemostep1;
+package com.mseclab.devfest.androidkeystoredemostep4;
 
 import java.io.IOException;
 import java.math.BigInteger;
@@ -20,6 +20,10 @@ import java.security.cert.CertificateException;
 import java.security.interfaces.RSAPrivateKey;
 import java.util.Calendar;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.security.auth.x500.X500Principal;
 
 import android.app.Activity;
@@ -55,7 +59,8 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 
 		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
+			getFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 	}
 
@@ -82,11 +87,12 @@ public class MainActivity extends Activity {
 	/**
 	 * A placeholder fragment containing a simple view.
 	 */
-	public static class PlaceholderFragment extends Fragment implements View.OnClickListener {
+	public static class PlaceholderFragment extends Fragment implements
+			View.OnClickListener {
 
 		private Button mGenChiaviButton;
-		private Button mFirmaButton;
-		private Button mVerificaButton;
+		private Button mCifraButton;
+		private Button mDecifraButton;
 		private TextView mDebugText;
 		private EditText mInData;
 		private EditText mOutData;
@@ -100,16 +106,20 @@ public class MainActivity extends Activity {
 		}
 
 		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
 
 			// Bottoni
-			mGenChiaviButton = (Button) rootView.findViewById(R.id.generate_button);
+			mGenChiaviButton = (Button) rootView
+					.findViewById(R.id.generate_button);
 			mGenChiaviButton.setOnClickListener(this);
-			mFirmaButton = (Button) rootView.findViewById(R.id.firma_button);
-			mFirmaButton.setOnClickListener(this);
-			mVerificaButton = (Button) rootView.findViewById(R.id.verifica_button);
-			mVerificaButton.setOnClickListener(this);
+			mCifraButton = (Button) rootView.findViewById(R.id.cifra_button);
+			mCifraButton.setOnClickListener(this);
+			mDecifraButton = (Button) rootView
+					.findViewById(R.id.decifra_button);
+			mDecifraButton.setOnClickListener(this);
 
 			// Text View
 			mInData = (EditText) rootView.findViewById(R.id.inDataText);
@@ -127,16 +137,116 @@ public class MainActivity extends Activity {
 				debug("Cliccato Genera chiavi");
 				generaChiavi();
 				break;
-			case R.id.firma_button:
-				debug("Cliccato Firma");
-				firmaData();
+			case R.id.cifra_button:
+				debug("Cliccato Cifra");
+				cifraData();
 				break;
-			case R.id.verifica_button:
-				debug("Cliccato Verifica");
-				verificaData();
+			case R.id.decifra_button:
+				debug("Cliccato Decifra");
+				decifraData();
 				break;
 
 			}
+		}
+
+		private KeyStore keyStore = null;
+		private KeyStore.Entry entry = null;
+
+		private void cifraData() {
+			// TODO Auto-generated method stub
+			String data = mInData.getText().toString();
+			debug("Stringa da cifrare:" + data);
+			byte[] rawData = data.getBytes();
+
+			KeyStore.PrivateKeyEntry entry = null;
+
+			try {
+				// Accesso alla chiave
+				keyStore = initKeyStore();
+				if (keyStore == null)
+					return;
+				entry = (KeyStore.PrivateKeyEntry) dammiElementoDalKeystore();
+				if (entry == null)
+					return;
+				PublicKey publicKeyEnc = ((KeyStore.PrivateKeyEntry) entry)
+						.getCertificate().getPublicKey();
+				Cipher encCipher = null;
+				byte[] ecryptedText = null;
+				encCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+				encCipher.init(Cipher.ENCRYPT_MODE, publicKeyEnc);
+				ecryptedText = encCipher.doFinal(rawData);
+				debug("Testo cifrato : " + ecryptedText.toString());
+				String encryptedDataBase64 = Base64.encodeToString(
+						ecryptedText, Base64.DEFAULT);
+				mOutData.setText(encryptedDataBase64);
+				debug("Testo cifrato Base64 : " + encryptedDataBase64);
+
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				debug(e.toString());
+			} catch (IllegalBlockSizeException e) {
+				debug(e.toString());
+			} catch (BadPaddingException e) {
+				debug(e.toString());
+			} catch (InvalidKeyException e) {
+				// TODO Auto-generated catch block
+				debug(e.toString());
+			} catch (NoSuchPaddingException e) {
+				debug(e.toString());
+			}
+
+		}
+
+		private void decifraData() {
+			// TODO Auto-generated method stub
+			KeyStore.PrivateKeyEntry entry = null;
+
+			byte[] data = mInData.getText().toString().getBytes();
+			byte[] stringEncrypted = mOutData.getText().toString().getBytes();
+
+			byte[] encrypted = null;
+			try {
+				encrypted = Base64.decode(stringEncrypted, Base64.DEFAULT);
+
+			} catch (IllegalArgumentException e) {
+				debug("String Base64 non valida");
+				return;
+			}
+			try {
+				// Accesso alla chiave
+				keyStore = initKeyStore();
+				if (keyStore == null)
+					return;
+				entry = (KeyStore.PrivateKeyEntry) dammiElementoDalKeystore();
+				if (entry == null)
+					return;
+				Cipher decCipher = null;
+				byte[] plainTextByte = null;
+				decCipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+				decCipher.init(Cipher.DECRYPT_MODE,
+						((KeyStore.PrivateKeyEntry) entry).getPrivateKey());
+				plainTextByte = decCipher.doFinal(encrypted);
+				String plainText = new String(plainTextByte);
+				if (plainText.equalsIgnoreCase(new String(data))){
+					debug("Testo decifrato: " + plainText);
+					mOutData.setText(plainText);
+				}else
+					debug("Errore nella decifratura");
+
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				debug(e.toString());
+			} catch (IllegalBlockSizeException e) {
+				debug(e.toString());
+			} catch (BadPaddingException e) {
+				debug(e.toString());
+			} catch (InvalidKeyException e) {
+				// TODO Auto-generated catch block
+				debug(e.toString());
+			} catch (NoSuchPaddingException e) {
+				debug(e.toString());
+			}
+
 		}
 
 		private void generaChiavi() {
@@ -151,9 +261,11 @@ public class MainActivity extends Activity {
 					Calendar notAfter = Calendar.getInstance();
 					notAfter.add(1, Calendar.YEAR);
 
-					android.security.KeyPairGeneratorSpec.Builder builder = new KeyPairGeneratorSpec.Builder(cx);
+					android.security.KeyPairGeneratorSpec.Builder builder = new KeyPairGeneratorSpec.Builder(
+							cx);
 					builder.setAlias(ALIAS);
-					String infocert = String.format("CN=%s, OU=%s", ALIAS, cx.getPackageName());
+					String infocert = String.format("CN=%s, OU=%s", ALIAS,
+							cx.getPackageName());
 					builder.setSubject(new X500Principal(infocert));
 					builder.setSerialNumber(BigInteger.ONE);
 					builder.setStartDate(notBefore.getTime());
@@ -163,15 +275,18 @@ public class MainActivity extends Activity {
 					KeyPairGenerator kpGenerator;
 					KeyPair kp = null;
 					try {
-						kpGenerator = KeyPairGenerator.getInstance("RSA", "AndroidKeyStore");
+						kpGenerator = KeyPairGenerator.getInstance("RSA",
+								"AndroidKeyStore");
 						kpGenerator.initialize(spec);
 						kp = kpGenerator.generateKeyPair();
 
 						publishProgress("Generated key pair : " + kp.toString());
 						PublicKey publickey = kp.getPublic();
 						PrivateKey privateKey = kp.getPrivate();
-						publishProgress("Formato della chiave pubblica : " + publickey.getFormat());
-						publishProgress("Algoritmo utilizzato : " + publickey.getAlgorithm());
+						publishProgress("Formato della chiave pubblica : "
+								+ publickey.getFormat());
+						publishProgress("Algoritmo utilizzato : "
+								+ publickey.getAlgorithm());
 						if (privateKey.getEncoded() == null)
 							publishProgress("Non possibile accedere direttamente alla chiave privata :-(");
 
@@ -198,101 +313,31 @@ public class MainActivity extends Activity {
 
 				@Override
 				protected void onPreExecute() {
-					progressdialog = ProgressDialog.show(getActivity(), "Please wait...", "Generating keys...");
+					progressdialog = ProgressDialog.show(getActivity(),
+							"Please wait...", "Generating keys...");
 				}
 
 			}.execute();
 
 		}
 
-		private void firmaData() {
-			String data = mInData.getText().toString();
-			debug("Stringa da firmare:" + data);
-			byte[] rawData = data.getBytes();
-
-			// Accesso alla chiave
-			KeyStore keyStore = initKeyStore();
-			if (keyStore == null)
-				return;
-
-			KeyStore.PrivateKeyEntry keyEntry;
-			byte[] signature = null;
+		// Load the key pair from the Android Key Store
+		private KeyStore.Entry dammiElementoDalKeystore() {
+			KeyStore.Entry entry = null;
 			try {
-				keyEntry = (KeyStore.PrivateKeyEntry) keyStore.getEntry(ALIAS, null);
-				RSAPrivateKey privKey = (RSAPrivateKey) keyEntry.getPrivateKey();
-
-				// Calcola firma
-				Signature s = Signature.getInstance(SIGN_ALG);
-				s.initSign(privKey);
-				s.update(rawData);
-				signature = s.sign();
+				entry = keyStore.getEntry(ALIAS, null);
 			} catch (NoSuchAlgorithmException e) {
-				debug(e.toString());
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			} catch (UnrecoverableEntryException e) {
-				debug(e.toString());
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			} catch (KeyStoreException e) {
-				debug(e.toString());
-			} catch (InvalidKeyException e) {
-				debug(e.toString());
-			} catch (SignatureException e) {
-				debug(e.toString());
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
+			return entry;
 
-			// OK!
-			if (signature != null) {
-				String signData = Base64.encodeToString(signature, Base64.DEFAULT);
-				mOutData.setText(signData);
-				debug("Firma Calcolata:\n" + signData);
-			}
-
-		}
-
-		private void verificaData() {
-			byte[] data = mInData.getText().toString().getBytes();
-			byte[] stringSignature = mOutData.getText().toString().getBytes();
-			byte[] signature = null;
-			try {
-				signature = Base64.decode(stringSignature, Base64.DEFAULT);
-			} catch (IllegalArgumentException e) {
-				debug("String Base64 non valida");
-				return;
-			}
-
-			// Accesso alla chiave
-			KeyStore keyStore = initKeyStore();
-			if (keyStore == null)
-				return;
-
-			KeyStore.PrivateKeyEntry keyEntry;
-			boolean isSignValid = false;
-			try {
-				keyEntry = (KeyStore.PrivateKeyEntry) keyStore.getEntry(ALIAS, null);
-				Certificate cert = keyEntry.getCertificate();
-
-				// Verifica firma
-				Signature s = Signature.getInstance(SIGN_ALG);
-				s.initVerify(cert);
-				s.update(data);
-				isSignValid = s.verify(signature);
-			} catch (NoSuchAlgorithmException e) {
-				debug(e.toString());
-			} catch (UnrecoverableEntryException e) {
-				debug(e.toString());
-			} catch (KeyStoreException e) {
-				debug(e.toString());
-			} catch (InvalidKeyException e) {
-				debug(e.toString());
-			} catch (SignatureException e) {
-				debug(e.toString());
-			}
-
-			if (isSignValid){
-				debug("Firma Valida");
-				Toast.makeText(getActivity(), "Firma Valida", Toast.LENGTH_LONG).show();	
-			}else{
-				debug("Firma Errata!");
-				Toast.makeText(getActivity(), "Firma Errata", Toast.LENGTH_LONG).show();	
-			}
 		}
 
 		private KeyStore initKeyStore() {
@@ -303,12 +348,12 @@ public class MainActivity extends Activity {
 
 			} catch (KeyStoreException e) {
 				debug("KeyStore Exception Error: " + e);
-			} catch (NoSuchAlgorithmException e1) {
-				debug(e1.toString());
-			} catch (CertificateException e1) {
-				debug(e1.toString());
-			} catch (IOException e1) {
-				debug(e1.toString());
+			} catch (NoSuchAlgorithmException e) {
+				debug(e.toString());
+			} catch (CertificateException e) {
+				debug(e.toString());
+			} catch (IOException e) {
+				debug(e.toString());
 			}
 			return keyStore;
 		}
